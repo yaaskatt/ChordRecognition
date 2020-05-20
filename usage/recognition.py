509775d73001331193341444a33b67109c530a71
yaaskatt <yaaskatt@yandex.ru>
2020-05-20 06:44:34 +0300

    recognition cleaned

--- usage/recognition.py
+++ usage/recognition.py
@@ -1,8 +1,6 @@
 from processing import datawork
 from usage import models
-from processing.paths import Path
 import numpy as np
-from keras.models import load_model
 import math
 
 def getChords(filepath):
@@ -11,11 +9,8 @@ def getChords(filepath):
     beat_chroma = datawork.reduceAll(np.split(chroma, beats, axis=1), 1)
 
     chord_changes = models.group(beat_chroma)
-    chords_cat = models.classify(beat_chroma, Path.beatClassifier)
+    chords_cat = models.classify(beat_chroma)
     result = np.zeros(chords_cat.shape[0])
-
-    model_f = load_model(Path.sequencer_fw)
-    model_b = load_model(Path.sequencer_bw)
 
     beat_chords, beat_accuracy = datawork.get_noncategorical(chords_cat)
 
@@ -24,16 +19,13 @@ def getChords(filepath):
         for i in range(19, len(chords_cat) - 20):
             seq1 = np.append(chords_cat[i-19:i], chord_changes[i-18:i+1].reshape(19, 1), axis=1)
             seq2 = np.append(chords_cat[i+19:i:-1], chord_changes[i+19:i:-1].reshape(19,1), axis=1)
-            result[i] = get_sequence_pred(seq1, seq2, chords_cat[i], model_f, model_b)
-            #print("classified:", beat_chords[i], "result:", datawork.get_chordNames(([result[i]]))[0])
+            result[i] = get_sequence_pred(seq1, seq2, chords_cat[i])
         for i in range(18, -1, -1):
             seq2 = np.append(chords_cat[i+19:i:-1], chord_changes[i+19:i:-1].reshape(19, 1), axis=1)
-            result[i] = get_backward_pred(seq2, chords_cat[i], model_b)
-            #print("classified:", beat_chords[i], "result:", datawork.get_chordNames(([result[i]]))[0])
+            result[i] = get_backward_pred(seq2, chords_cat[i])
         for i in range(len(chords_cat) - 20, len(chords_cat)):
             seq1 = np.append(chords_cat[i - 19:i], chord_changes[i - 18:i+1].reshape(19, 1), axis=1)
-            result[i] = get_forward_pred(seq1, chords_cat[i], model_f)
-            #print("classified:", beat_chords[i], "result:", datawork.get_chordNames(([result[i]]))[0])
+            result[i] = get_forward_pred(seq1, chords_cat[i])
 
         chords_cat = datawork.get_categorical_from_int(result)
 
@@ -64,9 +56,9 @@ def getChords(filepath):
     return grouped_time_chords
 
 
-def get_sequence_pred(seq1, seq2, classified, model_f, model_b):
-    chord_prob1 = models.predict(seq1, model_f)
-    chord_prob2 = models.predict(seq2, model_b)
+def get_sequence_pred(seq1, seq2, classified):
+    chord_prob1 = models.predict(seq1, 'f')
+    chord_prob2 = models.predict(seq2, 'b')
 
     chord_prob1 = (-chord_prob1).argsort()[:8]
     chord_prob2 = (-chord_prob2).argsort()[:8]
@@ -96,7 +88,6 @@ def get_sequence_pred(seq1, seq2, classified, model_f, model_b):
     probable = np.array(probable)
     change1, change2 = seq1[18][25], seq2[18][25]
 
-    #print(classified, chord_prob1, chord_prob2, probable)
 
     if classified[0] in probable:
         return classified[0]
@@ -110,12 +101,10 @@ def get_sequence_pred(seq1, seq2, classified, model_f, model_b):
         return probable[place1]
     return probable[0]
 
-def get_backward_pred(seq2, classified, model_b):
-    chord_prob2 = models.predict(seq2, model_b)
+def get_backward_pred(seq2, classified):
+    chord_prob2 = models.predict(seq2, 'b')
     chord_prob2 = (-chord_prob2).argsort()[:8]
     classified = (-classified).argsort()[:2]
-
-    #print(classified, print(chord_prob2))
 
     if classified[0] in chord_prob2:
         return classified[0]
@@ -123,12 +112,10 @@ def get_backward_pred(seq2, classified, model_b):
         return classified[1]
     return chord_prob2[0]
 
-def get_forward_pred(seq1, classified, model_f):
-    chord_prob1 = models.predict(seq1, model_f)
+def get_forward_pred(seq1, classified):
+    chord_prob1 = models.predict(seq1, 'f')
     chord_prob1 = (-chord_prob1).argsort()[:8]
     classified = (-classified).argsort()[:2]
-
-    #print((classified, chord_prob1))
 
     if classified[0] in chord_prob1:
         return classified[0]
